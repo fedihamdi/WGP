3c3cdacd1c07ba01123081d380c3704d219b9b70
Fedi Hamdi <fedihamdi.jr@gmail.com>
2021-06-28 17:03:45 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,10 +56,7 @@ module.exports = {
     },
     {
       resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `${__dirname}/content/posts`,
-      },
+      
     },
     {
       resolve: `gatsby-source-filesystem`,

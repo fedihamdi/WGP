7b089a699abf671d412b50104cdf3af52857fc53
Fedi Hamdi <fedihamdi.jr@gmail.com>
2023-05-27 23:46:51 +0200

    UPDATE: Analytics trackers

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,10 +12,28 @@ module.exports = {
 
   plugins: [
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-170P1KL6DC", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
+    }
+    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: 'UA-162203195-1',
+        trackingId: 'G-170P1KL6DC',
         head: true,
         anonymize: true,
         respectDNT: true,

14f1a5acccc24fcdc3826f5905dd0279dfdb180f
Fedi Hamdi <fedihamdi.jr@gmail.com>
2023-11-29 08:45:17 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,12 @@ module.exports = {
 
   plugins: [
     {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [`gatsby-remark-responsive-iframe`],
+      },
+    },
+    {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
